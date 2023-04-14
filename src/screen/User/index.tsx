1476211e31a99f83c140fc7fdc3925ead3a6d403
Aureo-Bueno <aureoalexandre@outlook.com>
2023-04-13 22:12:19 -0300

    feat: add list

--- src/screen/User/index.tsx
+++ src/screen/User/index.tsx
@@ -1,15 +1,14 @@
-import { Button, Text, View } from 'react-native';
+import { Text, FlatList, View } from 'react-native';
 import { IUser } from './@types';
 import { useQuery } from 'react-query';
 import { useGetUser } from '../../service';
-import { styles } from './styles/index';
+import { styles } from './styles';
 
 function User() {
   const { data, isLoading, isError } = useQuery<IUser[], Error>('users', useGetUser);
 
-
   return(
-    <View style={styles.container}>
+    <View style={styles.view}>
       {isLoading &&
         <Text>Carregando</Text>
       }
@@ -17,13 +16,22 @@ function User() {
       {isError &&
         <Text>Deu erro...</Text>
       }
-      {data?.map(user => (
-        <>
-          <View style={styles.mainCardView}>
-           <Text key={user.id}>{user.first_name} {user.last_name}</Text>
-          </View>
-        </>
-        ))}
+        <View style={styles.viewFlat}>
+          <FlatList 
+            data={data} 
+            keyExtractor={item => item.id}
+            renderItem={({item}) => <Text>{item.first_name}</Text>} 
+          >
+          </FlatList>
+        </View>
+
+        <View style={styles.viewMap}>
+          {data?.map((user) => (
+            <View>
+              <Text>{user.first_name}</Text>
+            </View>
+          ))}
+        </View>
     </View>
   );
 }
--- src/screen/User/styles/index.ts
+++ src/screen/User/styles/index.ts
@@ -1,27 +1,19 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-    backgroundColor: '#eaeaea',
-  },
-  mainCardView: {
-    height: 90,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'white',
-    borderRadius: 15,
-    shadowOffset: {width: 0, height: 0},
-    shadowOpacity: 1,
-    shadowRadius: 8,
-    elevation: 8,
+  view: {
+    display: 'flex',
+    marginTop: 15,
     flexDirection: 'row',
-    paddingLeft: 16,
-    paddingRight: 14,
-    marginTop: 6,
-    marginBottom: 6,
-    marginLeft: 16,
-    marginRight: 16,
+  },
+
+  viewFlat: {
+    width: '50%',
+    alignItems: 'center',
+  },
+
+  viewMap: {
+    width: '50%',
+    alignItems: 'center',
   },
 })
